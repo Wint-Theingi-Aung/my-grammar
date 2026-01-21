5deda5be8720ee7f6c2ae5f4dcff76db41883658
Wint Theingi Aung <shwewinttheingiaung@gmail.com>
2026-01-21 21:57:40 +0630

    Fix: Using absolute alias for supabase client

--- app/unit/[id]/page.tsx
+++ app/unit/[id]/page.tsx
@@ -1,4 +1,4 @@
-import { supabase } from '../../lib/supabaseClient';
+import { supabase } from '@/lib/supabaseClient';
 import { notFound } from 'next/navigation';
 
 export default async function UnitPage({ params }: { params: { id: string } }) {
