77cc132d71e4c38b400e25c2f45ec2c17530a6a3
Wint Theingi Aung <shwewinttheingiaung@gmail.com>
2026-01-21 20:29:00 +0630

    Remove my-grammar-main folder

--- my-grammar-main/lib/supabaseClient.js
+++
@@ -1,7 +0,0 @@
-// lib/supabaseClient.js
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://vwxgxazxjdnnfojwwhlu.supabase.co'
-const supabaseAnonKey = 'sb_publishable_HqVgWvVHzfNpH2J4_a3q4w_UdOjlvDC'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
