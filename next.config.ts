74389d2c3822a7eb60fbf4d533730d290bc9b0fe
Wint Theingi Aung <shwewinttheingiaung@gmail.com>
2026-01-25 19:28:29 +0630

    Fix: Remove invalid allowedDevOrigins from next config

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  outputFileTracingRoot: __dirname,
-
-  experimental: {
-    allowedDevOrigins: [
-      "http://localhost:3000",
-      "http://192.168.100.15:3000",
-    ],
-  },
+  /* config options here */
 };
 
 export default nextConfig;
